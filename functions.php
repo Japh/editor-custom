cacc77225987b21f118f8e9613db7969b09d4f7e
Japh <japh@x-team.com>
2014-07-20 22:59:37 +1000

    ASCII FTW!

--- functions.php
+++ functions.php
@@ -2,3 +2,55 @@
 /**
  * Editor Customisations
  */
+
+function wp_ascii_logo() {
+
+    $wp_ascii = '<!--' . "\n";
+    $wp_ascii .= '    PROUDLY POWERED BY...' . "\n";
+    $wp_ascii .= '' . "\n";
+    $wp_ascii .= '                              WWWWWWWWWWWWWWWWWWWW                              ' . "\n";
+    $wp_ascii .= '                         WWWWWWWWWWWWWWWWWWWWWWWWWWWWWW                         ' . "\n";
+    $wp_ascii .= '                    WWWWWWWWWWWWW              WWWWWWWWWWWWW                    ' . "\n";
+    $wp_ascii .= '                 WWWWWWWWW                            WWWWWWWWW                 ' . "\n";
+    $wp_ascii .= '              WWWWWWWW                                    WWWWWWWW              ' . "\n";
+    $wp_ascii .= '            WWWWWWWW                                        WWWWWWWW            ' . "\n";
+    $wp_ascii .= '          WWWWWWW                                             WWWWWWWW          ' . "\n";
+    $wp_ascii .= '        WWWWWWW                                             WWWWWWWWWWWW        ' . "\n";
+    $wp_ascii .= '      WWWWWWW                                             WWWWWWWWWWWWWWWW      ' . "\n";
+    $wp_ascii .= '     WWWWWWW                                              WWWWWWWWWWWWWWWWW     ' . "\n";
+    $wp_ascii .= '    WWWWWWWWWWWWWWWWWWWW   WWWWWWWWWWWWWWWWWWWWWWWW       WWWWWWWWWWWWWWWWWW    ' . "\n";
+    $wp_ascii .= '   WWWWWWWWWWWWWWWWWWWW      WWWWWWWWWWWWWWWWWWWWW        WWWWWWWWWWWWWWWWWWW   ' . "\n";
+    $wp_ascii .= '  WWWWWWWWWWWWWWWWW              WWWWWWWWWWWWWW            WWWWWWWWWWWWWWWWWWW  ' . "\n";
+    $wp_ascii .= ' WWWWWWWWWWWWWWWWWWW              WWWWWWWWWWWWW             WWWWWWWWWWWWWWWWWWW ' . "\n";
+    $wp_ascii .= 'WWWWWW  WWWWWWWWWWWW               WWWWWWWWWWWWW             WWWWWWWWWWWW WWWWWW' . "\n";
+    $wp_ascii .= 'WWWWWW  WWWWWWWWWWWWW              WWWWWWWWWWWWWW             WWWWWWWWWW  WWWWWW' . "\n";
+    $wp_ascii .= 'WWWWW    WWWWWWWWWWWWW              WWWWWWWWWWWWW              WWWWWWWWW   WWWWW' . "\n";
+    $wp_ascii .= 'WWWWW     WWWWWWWWWWWWW              WWWWWWWWWWWWW              WWWWWWW    WWWWW' . "\n";
+    $wp_ascii .= 'WWWW      WWWWWWWWWWWWW              WWWWWWWWWWWWWW             WWWWWWW     WWWW' . "\n";
+    $wp_ascii .= 'WWWW       WWWWWWWWWWWWW              WWWWWWWWWWWWW             WWWWWW      WWWW' . "\n";
+    $wp_ascii .= 'WWWW        WWWWWWWWWWWWW            WWWWWWWWWWWWWWW            WWWWW       WWWW' . "\n";
+    $wp_ascii .= 'WWWW         WWWWWWWWWWWW           WWWWWWWWWWWWWWWWW          WWWWWW       WWWW' . "\n";
+    $wp_ascii .= 'WWWWW        WWWWWWWWWWWWW          WWWWWWWWWWWWWWWWW         WWWWWW       WWWWW' . "\n";
+    $wp_ascii .= 'WWWWW         WWWWWWWWWWWWW        WWWWW  WWWWWWWWWWWW        WWWWW        WWWWW' . "\n";
+    $wp_ascii .= 'WWWWWW         WWWWWWWWWWWW       WWWWW   WWWWWWWWWWWWW      WWWWWW       WWWWWW' . "\n";
+    $wp_ascii .= ' WWWWW         WWWWWWWWWWWWW      WWWW     WWWWWWWWWWWW      WWWWW        WWWWW ' . "\n";
+    $wp_ascii .= ' WWWWWW         WWWWWWWWWWWWW    WWWW       WWWWWWWWWWWW    WWWWW        WWWWWW ' . "\n";
+    $wp_ascii .= '  WWWWWW         WWWWWWWWWWWW   WWWWW        WWWWWWWWWWWW  WWWWW        WWWWWW  ' . "\n";
+    $wp_ascii .= '   WWWWWW         WWWWWWWWWWWW WWWWW         WWWWWWWWWWWWW WWWWW       WWWWWW   ' . "\n";
+    $wp_ascii .= '    WWWWWW         WWWWWWWWWWWWWWWW           WWWWWWWWWWWWWWWWW       WWWWWW    ' . "\n";
+    $wp_ascii .= '     WWWWWWW       WWWWWWWWWWWWWWWW            WWWWWWWWWWWWWWW      WWWWWWW     ' . "\n";
+    $wp_ascii .= '       WWWWWWW      WWWWWWWWWWWWWW              WWWWWWWWWWWWWW    WWWWWWW       ' . "\n";
+    $wp_ascii .= '        WWWWWWWW     WWWWWWWWWWWW               WWWWWWWWWWWWW   WWWWWWWW        ' . "\n";
+    $wp_ascii .= '          WWWWWWWW    WWWWWWWWWWW                WWWWWWWWWWWW WWWWWWWW          ' . "\n";
+    $wp_ascii .= '            WWWWWWWW  WWWWWWWWWW                  WWWWWWWWWWWWWWWWWW            ' . "\n";
+    $wp_ascii .= '               WWWWWWWWWWWWWWWW                    WWWWWWWWWWWWWW               ' . "\n";
+    $wp_ascii .= '                  WWWWWWWWWWWWW                    WWWWWWWWWWW                  ' . "\n";
+    $wp_ascii .= '                      WWWWWWWWWWWWWW        WWWWWWWWWWWWWW                      ' . "\n";
+    $wp_ascii .= '                          WWWWWWWWWWWWWWWWWWWWWWWWWWWW                          ' . "\n";
+    $wp_ascii .= '                               WWWWWWWWWWWWWWWWWW                               ' . "\n";
+    $wp_ascii .= '-->' . "\n";
+
+    echo $wp_ascii;
+
+}
+add_action( 'wp_head', 'wp_ascii_logo' );
